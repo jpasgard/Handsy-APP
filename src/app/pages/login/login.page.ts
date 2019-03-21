a19cc8b84ea1d34c77fa418382b28f178df12b07
rafaelmatheus <rafaelmatheusdecastro@hotmail.com.br>
2019-03-21 09:59:20 -0300

    App - Tela de login
    Adicionado tela de login com validações mas sem o design

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,19 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage{
+  public formGroup: FormGroup;
 
-  constructor(private nav: NavController) { }
+  constructor(private nav: NavController, private formBiuld: FormBuilder) {
 
-  ngOnInit() {
-  }
-  click(){
-    this.nav.navigateForward('home');
-  }
+    this.formGroup = this.formBiuld.group({
+      email: [null, Validators.compose([
+        Validators.required,
+        Validators.email
+      ])],
+
+      senha: [null, Validators.compose([
+        Validators.required
+        
+      ])]
+
+    });
+   }
+
+   entrou(){
+    console.log(this.formGroup.invalid);
+    console.log(this.formGroup.value);
+   }
 
 }
+function newFunction() {
+  return this;
+}
+
+
+
