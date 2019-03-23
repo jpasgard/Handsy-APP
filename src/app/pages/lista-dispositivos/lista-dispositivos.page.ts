278a51c095bf5b4ec013d2b46f31b8710e8d5f54
rafaelmatheus <rafaelmatheusdecastro@hotmail.com.br>
2019-03-23 17:06:01 -0300

    App qrcode
    Adicionado leitura de qrcode

--- src/app/pages/lista-dispositivos/lista-dispositivos.page.ts
+++ src/app/pages/lista-dispositivos/lista-dispositivos.page.ts
@@ -26,19 +26,39 @@ export class ListaDispositivosPage implements OnInit {
     estado: false
   }
 ];
-  constructor(private activated: ActivatedRoute, private bar: QRScanner) {}
+  constructor(private activated: ActivatedRoute, private qrScanner: QRScanner) {}
 
   ngOnInit(): void {
    this.id =  this.activated.snapshot.params.id;
     console.log(this.id);
+    console.log("entrou");
   }
 
   cadastrarDispositivo(){
-    let scanSub = this.bar.scan().subscribe((text: string)=>{
-      console.log(text);
-      this.bar.hide();
-     scanSub.unsubscribe();
-    },error=> console.log(error));
-  }
+ 
+this.qrScanner.prepare()
+.then((status: QRScannerStatus) => {
+   if (status.authorized) {
+     // camera permission was granted
 
-}
+
+     // start scanning
+     let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+       console.log('Scanned something', text);
+
+       this.qrScanner.hide(); // hide camera preview
+       scanSub.unsubscribe(); // stop scanning
+     });
+
+   } else if (status.denied) {
+     // camera permission was permanently denied
+     // you must use QRScanner.openSettings() method to guide the user to the settings page
+     // then they can grant the permission from there
+   } else {
+     // permission was denied, but not permanently. You can ask for permission again at a later time.
+   }
+})
+.catch((e: any) => console.log('Error is', e));
+
+
+  }}
