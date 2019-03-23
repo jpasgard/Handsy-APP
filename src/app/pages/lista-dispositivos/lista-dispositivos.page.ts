b7a0340152bba18ef11263047baa8cf10c830d33
rafaelmatheus <rafaelmatheusdecastro@hotmail.com.br>
2019-03-23 17:50:32 -0300

    App lista dispositivos
    Adicionado metodo para recuperar o valor do toggle do dispositivo, informando se está ou não ligado

--- src/app/pages/lista-dispositivos/lista-dispositivos.page.ts
+++ src/app/pages/lista-dispositivos/lista-dispositivos.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { dispositivoDto } from 'src/app/model/dispositivo.dto';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
-import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -26,7 +26,9 @@ export class ListaDispositivosPage implements OnInit {
     estado: false
   }
 ];
-  constructor(private activated: ActivatedRoute, private qrScanner: QRScanner) {}
+  constructor(private activated: ActivatedRoute, 
+    private barcodeScanner: BarcodeScanner,
+    private alertController: AlertController) {}
 
   ngOnInit(): void {
    this.id =  this.activated.snapshot.params.id;
@@ -35,30 +37,23 @@ export class ListaDispositivosPage implements OnInit {
   }
 
   cadastrarDispositivo(){
- 
-this.qrScanner.prepare()
-.then((status: QRScannerStatus) => {
-   if (status.authorized) {
-     // camera permission was granted
-
-
-     // start scanning
-     let scanSub = this.qrScanner.scan().subscribe((text: string) => {
-       console.log('Scanned something', text);
-
-       this.qrScanner.hide(); // hide camera preview
-       scanSub.unsubscribe(); // stop scanning
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+        this.presentAlert(err);
      });
+}
+  async presentAlert(e: any) {
+    const alert = await this.alertController.create({
+      header: 'Erro',
+      message: e,
+      buttons: ['OK']
+    });
 
-   } else if (status.denied) {
-     // camera permission was permanently denied
-     // you must use QRScanner.openSettings() method to guide the user to the settings page
-     // then they can grant the permission from there
-   } else {
-     // permission was denied, but not permanently. You can ask for permission again at a later time.
-   }
-})
-.catch((e: any) => console.log('Error is', e));
+    await alert.present();
+  }
+  captureToggle(valor: any): boolean{
+    return valor.detail.checked;
+    }
 
-
-  }}
+}
